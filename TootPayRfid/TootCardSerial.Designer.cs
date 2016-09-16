19ba3794ad318eee4e8ba7bf1258000e758751fe
Klarizon Emar Motol <klarizonemar@gmail.com>
2016-09-17 07:52:06 +0800

    Add project files.

---
+++ TootPayRfid/TootCardSerial.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace TootPayRfid
+{
+    partial class TootCardSerial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.rfidSerialPort = new System.IO.Ports.SerialPort(this.components);
+            this.dataLabel = new System.Windows.Forms.Label();
+            this.dataTextBox = new System.Windows.Forms.TextBox();
+            this.logsRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // rfidSerialPort
+            // 
+            this.rfidSerialPort.PortName = "COM4";
+            this.rfidSerialPort.RtsEnable = true;
+            this.rfidSerialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.rfidSerialPort_DataReceived);
+            // 
+            // dataLabel
+            // 
+            this.dataLabel.AutoSize = true;
+            this.dataLabel.Location = new System.Drawing.Point(15, 15);
+            this.dataLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.dataLabel.Name = "dataLabel";
+            this.dataLabel.Size = new System.Drawing.Size(63, 25);
+            this.dataLabel.TabIndex = 0;
+            this.dataLabel.Text = "Data:";
+            // 
+            // dataTextBox
+            // 
+            this.dataTextBox.Location = new System.Drawing.Point(87, 12);
+            this.dataTextBox.Name = "dataTextBox";
+            this.dataTextBox.ReadOnly = true;
+            this.dataTextBox.Size = new System.Drawing.Size(561, 31);
+            this.dataTextBox.TabIndex = 1;
+            this.dataTextBox.TextChanged += new System.EventHandler(this.dataTextBox_TextChanged);
+            // 
+            // logsRichTextBox
+            // 
+            this.logsRichTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logsRichTextBox.Location = new System.Drawing.Point(12, 49);
+            this.logsRichTextBox.Name = "logsRichTextBox";
+            this.logsRichTextBox.ReadOnly = true;
+            this.logsRichTextBox.Size = new System.Drawing.Size(636, 347);
+            this.logsRichTextBox.TabIndex = 2;
+            this.logsRichTextBox.Text = "";
+            // 
+            // TootCardSerial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 408);
+            this.Controls.Add(this.logsRichTextBox);
+            this.Controls.Add(this.dataTextBox);
+            this.Controls.Add(this.dataLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
+            this.Name = "TootCardSerial";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Toot Card Scan";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TootCardSerial_FormClosing);
+            this.Load += new System.EventHandler(this.TootCardSerial_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort rfidSerialPort;
+        private System.Windows.Forms.Label dataLabel;
+        private System.Windows.Forms.TextBox dataTextBox;
+        private System.Windows.Forms.RichTextBox logsRichTextBox;
+    }
+}
+
---
+++ TootPayRfid/TootCardSerial.cs
@@ -0,0 +1,158 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TootPayRfid
+{
+    delegate void IncommingDelegate(string text);
+
+    public partial class TootCardSerial : Form
+    {
+        IncommingDelegate incommingDelegate;
+        int dataSize = 44;
+
+        public TootCardSerial()
+        {
+            InitializeComponent();
+            incommingDelegate = new IncommingDelegate(appendText);
+        }
+
+        void appendText(string text)
+        {
+            string _text = this.dataTextBox.Text + text;
+            this.dataTextBox.Text = _text;
+        }
+
+        private void rfidSerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort incomming = (SerialPort)sender;
+            int incommingBytes = incomming.BytesToRead;
+
+            char[] incommingBuffer = new char[incommingBytes];
+            incomming.Read(incommingBuffer, 0, incommingBytes);
+
+            String tempString = new String(incommingBuffer);
+            this.BeginInvoke(incommingDelegate, tempString);
+        }
+
+        private void dataTextBox_TextChanged(object sender, EventArgs e)
+        {
+            string tag = (sender as TextBox).Text;
+
+            if (tag.Length == dataSize)
+            {
+                String data = new String(tag.ToCharArray());
+
+                logsRichTextBox.AppendText(string.Format("\n[{0}]: Formatting data...", timestamp()));
+                string _data = data.TrimEnd();
+                
+                logsRichTextBox.AppendText(string.Format("\n[{0}]: Data received: {1}.", timestamp(), _data));
+                logsRichTextBox.AppendText(string.Format("\n[{0}]: Data received index count: {1}.", timestamp(), _data.Length));
+
+                logsRichTextBox.AppendText(string.Format("\n[{0}]: Getting serial from data...", timestamp()));
+                string _tag = _data.Split(' ').Last().TrimEnd();
+
+                logsRichTextBox.AppendText(string.Format("\n[{0}]: Serial: {1}.", timestamp(), _tag));
+                logsRichTextBox.AppendText(string.Format("\n[{0}]: Serial index count: {1}.", timestamp(), _tag.Length));
+                patchTagToServer(_tag);
+            }
+        }
+
+        private void patchTagToServer(string tag)
+        {
+            try
+            {
+                logsRichTextBox.AppendText(string.Format("\n[{0}]: Patching serial to tootpay server...", timestamp()));
+                MySqlConnection mySqlConnection = new MySqlConnection();
+                mySqlConnection.ConnectionString = Properties.Resources.mySqlConnectionString;
+                mySqlConnection.Open();
+
+                MySqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+                mySqlCommand.CommandText = "insert into serials(tag) values(@tag)";
+                mySqlCommand.Parameters.AddWithValue("@tag", tag);
+
+                if (mySqlCommand.ExecuteNonQuery() == 1)
+                {
+                    logsRichTextBox.AppendText(string.Format("\n[{0}]: Row inserted!", timestamp()));
+                }
+                mySqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                logsRichTextBox.AppendText(string.Format("\n[{0}]: Error! {1}", timestamp(), ex.Message));
+            }
+            finally
+            {
+                closePort();
+                dataTextBox.ResetText();
+                logsRichTextBox.AppendText(string.Format("\n[{0}]: Data cleared!", timestamp()));
+                openPort(true);
+            }
+        }
+
+        private string timestamp()
+        {
+            return DateTime.Now.ToString();
+        }
+
+        private void openPort(bool newLine)
+        {
+            try
+            {
+                if (!rfidSerialPort.IsOpen)
+                {
+                    rfidSerialPort.Open();
+
+                    if (newLine)
+                    {
+                        logsRichTextBox.AppendText(string.Format("\n[{0}]: Port opened!", timestamp()));
+                    }
+                    else
+                    {
+                        logsRichTextBox.AppendText(string.Format("[{0}]: Port opened!", timestamp()));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                logsRichTextBox.AppendText(string.Format("\n[{0}]: Error! {1}", timestamp(), ex.Message));
+            }
+            logsRichTextBox.AppendText(string.Format("\n[{0}]: Listening on port {1}...", timestamp(), rfidSerialPort.PortName));
+        }
+
+        private void closePort()
+        {
+            try
+            {
+                if (rfidSerialPort.IsOpen)
+                {
+                    rfidSerialPort.Close();
+                    logsRichTextBox.AppendText(string.Format("\n[{0}]: Port closed!", timestamp()));
+                }
+            }
+            catch (Exception ex)
+            {
+                logsRichTextBox.AppendText(string.Format("\n[{0}]: Error! {1}", timestamp(), ex.Message));
+            }
+        }
+
+        private void TootCardSerial_Load(object sender, EventArgs e)
+        {
+            openPort(false);
+        }
+
+        private void TootCardSerial_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            closePort();
+        }
+    }
+}
